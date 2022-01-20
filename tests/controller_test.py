b26b074ccbf30d716ade5e383b4680ab7d7be86c
LARJAEG <lars.jaeger@daimler.com>
2022-01-20 13:39:01 +0100

    fix test

--- tests/controller_test.py
+++ tests/controller_test.py
@@ -7,12 +7,12 @@ from input_framework.imu_controller import IMUController
 from input_framework.interface import ThresholdType, TriggerMode
 
 
-class Icon(Enum):
+"""class Icon(Enum):
     up = np.full((16, 16, 3), 100)
     down = np.full((16, 16, 3), 200)
-
+"""
 
 if __name__ == "main":
     controller = IMUController(TriggerMode.CHECK_LOOP)
-    controller.register_trigger(OutputFramework.setWindow, {'ausgabe': Icon.up}, input.rot_x, 0.1, ThresholdType.HIGHER)
-    controller.register_trigger(OutputFramework.setWindow, {'ausgabe': Icon.down}, input.rot_x, -0.1, ThresholdType.LOWER)
+    controller.register_trigger(OutputFramework.setWindow, {'ausgabe': np.full((16, 16, 3), 100)}, input.rot_x, 0.1, ThresholdType.HIGHER)
+    controller.register_trigger(OutputFramework.setWindow, {'ausgabe': np.full((16, 16, 3), 200)}, input.rot_x, -0.1, ThresholdType.LOWER)
