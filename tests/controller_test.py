2ab91a55b55b7bd007c3bf61f287872acb1b7c7a
LARJAEG <lars.jaeger@daimler.com>
2022-01-21 09:46:13 +0100

    fix legacy notation

--- tests/controller_test.py
+++ tests/controller_test.py
@@ -1,11 +1,7 @@
-from enum import Enum
-
 import numpy as np
-from output_framework.output_framework import OutputFramework
-
 from input_framework.imu_controller import IMUController
 from input_framework.interface import ThresholdType, TriggerMode
-
+from output_framework.output_framework import OutputFramework
 
 """class Icon(Enum):
     up = np.full((16, 16, 3), 100)
@@ -14,5 +10,7 @@ from input_framework.interface import ThresholdType, TriggerMode
 
 if __name__ == "__main__":
     controller = IMUController(TriggerMode.CHECK_LOOP)
-    controller.register_trigger(OutputFramework.setWindow, {'ausgabe': np.full((16, 16, 3), 100)}, input.rot_x, 0.1, ThresholdType.HIGHER)
-    controller.register_trigger(OutputFramework.setWindow, {'ausgabe': np.full((16, 16, 3), 200)}, input.rot_x, -0.1, ThresholdType.LOWER)
+    controller.register_trigger(OutputFramework.setWindow, {'ausgabe': np.full((16, 16, 3), 100)}, controller.rot_x,
+                                0.1, ThresholdType.HIGHER)
+    controller.register_trigger(OutputFramework.setWindow, {'ausgabe': np.full((16, 16, 3), 200)}, controller.rot_x,
+                                -0.1, ThresholdType.LOWER)
